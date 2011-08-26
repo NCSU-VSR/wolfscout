7863c1e9b0606efdc2c898f9637991788ea048d5
Chris King <chris.king@kilncode.com>
2011-08-26 16:11:47 -0400

    making settings.py more modular

--- settings/settings.py
+++ settings/settings.py
@@ -45,7 +45,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '../'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '../'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
