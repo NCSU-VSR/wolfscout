b4c8a9e5a0b135e9e9b364aea381b01101fb401e
Chris King <chris.king@kilncode.com>
2011-08-28 17:05:32 -0400

    I forgot to stage the files... Kind of a dumb move

--- settings/sample.py
+++ settings/sample.py
@@ -1,2 +1,18 @@
-__author__ = 'chris'
-  
+"""
+Use Sample.py as a guide for your local settings.py file that you define according to the documentation.
+Any fields that need to be customized for a local deployment can be found here.
+"""
+
+from common import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
