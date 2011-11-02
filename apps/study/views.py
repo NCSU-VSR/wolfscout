bc9daeb69c031f576a4d1f18a2f555b7e4c38a32
Scott Gentry <sagentry@ncsu.edu>
2011-11-01 23:46:00 -0400

    LOTS O FORMATTING for presentztion

--- apps/study/views.py
+++ apps/study/views.py
@@ -39,7 +39,7 @@ def edit(request, theStudyID):
     siteDictionary['title'] = "Edit Study"
     study = get_object_or_404(Study, pk=theStudyID)
     if request.method == 'POST':
-        form = StudyForm(request.POST)
+        form = StudyForm(request.POST, instance=Study.objects.get(pk=theStudyID))
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/studies')
@@ -47,6 +47,16 @@ def edit(request, theStudyID):
         form = StudyForm(error_class=DivErrorList, auto_id='id_%s', instance=study)
     siteDictionary['form'] = form
     return render_to_response('editStudy.html', siteDictionary, context_instance=RequestContext(request))
+
+@login_required()  
+def delete(request, theStudyID):
+    """
+    """
+    siteDictionary = {}
+    siteDictionary['title'] = "Edit Study"
+    study = get_object_or_404(Study, pk=theStudyID)
+    study.delete()
+    return render_to_response('studies.html', siteDictionary, context_instance=RequestContext(request))   
     
 @login_required()  
 def add(request):
