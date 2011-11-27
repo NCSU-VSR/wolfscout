870d17698db6c4448165ef9b03b626733f14701e
Chris King <chris@pruvop.com>
2011-11-27 15:31:17 -0500

    updated processor to grab all weather station data first

--- apps/crawler/gpscollar/fileProcessor.py
+++ apps/crawler/gpscollar/fileProcessor.py
@@ -4,11 +4,11 @@ import os
 import glob
 from apps.crawler.gpscollar.collar import CollarParser
 from apps.crawler.gpscollar.models import Collar, CollarData
-
+from apps.crawler.cronos.views import scrapeStations
 from apps.crawler.gpscollar.views import *
 PATH_TO_SAMPLE_DATA = "/opt/webapps/ncsu/wolfscout/sample_data/"
 
-
+scrapeStations()
 for infile in glob.glob( os.path.join(PATH_TO_SAMPLE_DATA, '*.TXT') ):
     cp = CollarParser(infile)
     cp.processCSVIntoDatabase()
