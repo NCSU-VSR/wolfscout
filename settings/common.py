52b23bd43309532233db02a51619eed0b8e20905
ubuntu <ubuntu@eb2-1221-sdd01.csc.ncsu.edu>
2011-11-27 14:01:41 -0500

    corrected gunicorn configurations

--- settings/common.py
+++ settings/common.py
@@ -45,7 +45,7 @@ MEDIA_ROOT = os.path.join(PROJECT_DIR,  'media/')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/frank_media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -58,7 +58,7 @@ STATIC_URL = '/static/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = '/media/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
