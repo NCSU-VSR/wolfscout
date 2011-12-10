d53ea2cd447f3733f2fbc4d6caa43a8259ba3ddf
Chris King <chris.king@kilncode.com>
2011-12-10 16:15:36 -0500

    added choices for animal age

--- apps/wildlife/models.py
+++ apps/wildlife/models.py
@@ -21,9 +21,17 @@ class Animal(models.Model):
     Animals that are being tracked in some fashion.
     Possibilities include a collar GPS unit, weather info, or custom sensors
     """
+
+    AGE_CHOICES = (
+        ('FR', 'Freshman'),
+        ('SO', 'Sophomore'),
+        ('JR', 'Junior'),
+        ('SR', 'Senior'),
+        ('GR', 'Graduate'),
+        )
     collar = models.ForeignKey(Collar, null=True, blank=True)
     common_name = models.CharField(max_length=50, unique=True)
-    age = models.CharField(max_length=50, null=True, blank=True)
+    age = models.CharField(max_length=50, null=True, blank=True, choices=AGE_CHOICES)
     sex = models.CharField(max_length=50, null=True, blank=True)
     species = models.ForeignKey(Species, null=True, blank=True)
 
