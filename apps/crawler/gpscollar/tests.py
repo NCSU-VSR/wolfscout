a99a8e11d62a898243fc262d8acc5a35796f85ae
Bo Chulindra <bo@bo.com>
2011-12-11 18:13:13 -0500

    tested

--- apps/crawler/gpscollar/tests.py
+++ apps/crawler/gpscollar/tests.py
@@ -104,7 +104,6 @@ class CollarTestCases(TestCase):
         #file = File(f)
         opened_file = File(open("sample_data/GSM999999999.TXT","rb"))
         filename = views.write_file_to_disk(opened_file)
-        opened_file.close()
         self.assertTrue(os.path.exists(filename))
 
     def test_uploadCSVToProcess(self):
